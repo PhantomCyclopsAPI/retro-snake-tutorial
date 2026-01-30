8b5eeffb97db351af9f0c949eb514db873b0dcb1
PhantomCyclops <astrospock0032@gmail.com>
2026-01-29 17:09:07 -0800

    Initialize game.js with basic setup and imports

---
+++ JS_Codes/snake/game.js
@@ -0,0 +1,9 @@
+/**
+ * EP01: The Render Engine
+ * We will start coding here.
+ */
+import p5 from 'p5';
+import { GRID_SIZE, TILE_SIZE } from './config.js';
+
+// TODO: Create p5 instance
+console.log('Game engine ready.');
