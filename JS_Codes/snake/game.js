54b67d1945c487ed05716ab1c3ea20ce3f3e3c5b
RetroIndexGames <astrospock0032@gmail.com>
2026-02-03 11:58:23 -0800

    Clean up game.js by removing comments and imports
    Removed initial comments and import statements from game.js.

--- JS_Codes/snake/game.js
+++ JS_Codes/snake/game.js
@@ -1,9 +1,1 @@
-/**
- * EP01: The Render Engine
- * We will start coding here.
- */
-import p5 from 'p5';
-import { GRID_SIZE, TILE_SIZE } from './config.js';
-
-// TODO: Create p5 instance
-console.log('Game engine ready.');
+//EP01
